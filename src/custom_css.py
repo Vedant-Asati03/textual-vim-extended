18e11460dd72443ea4b4af1e169353415b4c5504
vedant-asati03 <vedant.asati03@gmail.com>
2025-02-16 14:03:30 +0530

    Add custom CSS class for styling borders and titles

---
+++ src/custom_css.py
@@ -0,0 +1,11 @@
+class CSS:
+
+    def set_custom_css(self):
+        self.styles.border = ("blank", "#0C0C0C")
+
+        self.styles.border_title_align = "right"
+        self.styles.border_title_color = "#F5F5F5"
+        self.styles.border_title_background = "#3C3D37"
+
+        self.styles.border_subtitle_align = "right"
+        self.styles.border_subtitle_style = "bold"
